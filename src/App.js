671e721422d706ee055c5eef476b7cf24185175f
EugeBr <eugebordenave@gmail.com>
2022-11-08 16:37:42 +0100

    detele todos prop

--- src/App.js
+++ src/App.js
@@ -31,7 +31,7 @@ function App() {
     });
   };
 
-  const completeTodos = (text) => {
+  const completeTodo = (text) => {
     const todoIndex = todos.findIndex(todo => todo.text === text);
 
     const newTodos = [...todos];
@@ -45,6 +45,13 @@ function App() {
     //*mejor forma de hacerlo
     newTodos[todoIndex].completed = true;
 
+    setTodos(newTodos);
+  };
+
+  const deleteTodo = (text) => {
+    const todoIndex = todos.findIndex(todo => todo.text === text);
+    const newTodos = [...todos];
+    newTodos.splice(todoIndex, 1);
     setTodos(newTodos);
   };
 
@@ -64,7 +71,8 @@ function App() {
             key={todo.text}
             text={todo.text}
             completed={todo.completed}
-            onComplete={() => completeTodos(todo.text)}
+            onComplete={() => completeTodo(todo.text)}
+            onDelete={() => deleteTodo(todo.text)}
           />
         ))}
       </TodoList>
